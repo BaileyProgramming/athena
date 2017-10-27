83ec7e1404822bc63c93bed7b9b8921c241c7451
beetle442002 <beetle442002@gmail.com>
2017-10-26 22:15:46 -0500

    updated index

--- web/index.php
+++ web/index.php
@@ -4,14 +4,19 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Untitled Document</title>
 <link href="athena.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+   
 </head>
 
 <body>
 <div class="grid">
 	<h1>Athena</h1>
 	<div class="signon">
-		<section id="signin">Content for  id "signin" Goes Here</section>
-		<section id="sso">Content for  id "sso" Goes Here</section>
+		<section id="signin"><form><label for="user">User:</label><input name="user" type="text" id="user"><br /><label for="password">Password:</label><input name="password" type="password" id="password"></form></section>
+		<section id="sso">
+			<div class="sso-g" style="font-family: 'Roboto'; text-align: center;"><span class="sso-g-img"></span><span class="sso-g-label">Sign in with Google</span></div>
+		  Content for  id "sso" Goes Here
+		</section>
 	</div>
 	<footer id="footer">Athena &copy;2017 BaileyProgramming.com </footer>
 </div>
